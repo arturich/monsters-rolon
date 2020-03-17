ec5ab7096c7d2422ef080ed122d9ac25176cd5bf
Arturo Reyes Fernández <arturo.reyes@citalin.com>
2020-03-16 21:59:38 -0600

    Updates

---
+++ precache-manifest.e284c538d5b48a8ca6b62a49cc20d209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b862f3bf89290166d168d7a5f0c2e84",
+    "url": "/monsters-rolon.git/index.html"
+  },
+  {
+    "revision": "ba8bfbbc560ed108d3c4",
+    "url": "/monsters-rolon.git/static/css/main.f00aa21a.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolon.git/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolon.git/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba8bfbbc560ed108d3c4",
+    "url": "/monsters-rolon.git/static/js/main.6c3bab7e.chunk.js"
+  },
+  {
+    "revision": "9b98e8cda4a6eddd17ab",
+    "url": "/monsters-rolon.git/static/js/runtime-main.d0611374.js"
+  }
+]);
